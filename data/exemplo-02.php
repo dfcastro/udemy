33c3dafdaa5341611aca990eb45758aa00be65b4
Daniel Frank <dfcastro@outlook.com.br>
2020-09-22 15:04:11 -0300

    Adicionando data

---
+++ data/exemplo-02.php
@@ -0,0 +1,6 @@
+<?php 
+
+$ts= strtotime("2001-09-11");
+$ts= strtotime("now");
+echo date("d/m/Y", $ts);
+ ?>
---
+++ data/exemplo-03.php
@@ -0,0 +1,8 @@
+<?php
+
+setlocale(LC_ALL, "pt_BR","pt_BR.utf-8","ptb.utf-8");
+
+echo ucwords(strftime("%A %B")); //trazer dia da semana e mês em português
+
+
+ ?>
