7fe86b451da778994297677222601a4a96535cf7
Daniel Frank <dfcastro@outlook.com.br>
2020-09-27 10:02:53 -0300

    Adicionando insert e update

--- DAO/class/Usuario.php
+++ DAO/class/Usuario.php
@@ -56,17 +56,86 @@ class Usuario{
 
 		if(count($results)>0)
 		{
-			$row = $results[0];
-
-			$this->setIdUsuario($row['idusuario']);
-			$this->setDeslogin($row['deslogin']);
-			$this->setDessenha($row['dessenha']);
-			$this->setDtCadastro(new DateTime ($row['dtcadastro']));
+			$this->setData($results[0]);
 		}
 
 	}
 
-	public function __toString(){
+	public static function  getList()
+	{
+		$sql = new Sql();
+		return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin;");
+	}
+
+	public static function search($login)
+	{
+		$sql = new Sql();
+		return $sql->select("SELECT * FROM tb_usuarios where deslogin LIKE :SEARCH order by deslogin", array(
+':SEARCH'=>"%".$login."%"
+		));
+	}
+
+	public function login($login, $password)
+	{
+		$sql = new Sql;
+		$results= $sql->select("SELECT * FROM tb_usuarios WHERE deslogin= :LOGIN AND dessenha = :PASSWORD", array(
+			":LOGIN"=>$login,
+			":PASSWORD"=>$password
+		));
+
+		if(count($results)>0)
+		{
+			
+			$this->setData($results[0]);
+		}
+		else
+		{
+			throw new Exception('Login ou senha invalidos');
+		}
+
+	}
+
+	public function setData($data)
+	{
+			$this->setIdUsuario($data['idusuario']);
+			$this->setDeslogin($data['deslogin']);
+			$this->setDessenha($data['dessenha']);
+			$this->setDtCadastro(new DateTime ($data['dtcadastro']));
+	}
+
+	public function insert()
+	{
+		$sql = new Sql();
+		$results= $sql->select("CALL sp_usuarios_insert(:LOGIN, :PASSWORD)", array(
+			':LOGIN'=>$this->getDeslogin(),
+			':PASSWORD'=>$this->getDessenha()
+		));
+		if(count($results)>0)
+		{
+			$this->setData($results[0]);
+		}
+	}
+
+	public function update($login,$password)
+	{
+		$this->setDeslogin($login);
+		$this->setDessenha($password);
+		$sql = new Sql();
+		$sql->query("UPDATE tb_usuarios SET deslogin= :LOGIN, dessenha = :PASSWORD WHERE idusuario = :ID", array(
+			':LOGIN'=>$this->getDeslogin(),
+			':PASSWORD'=>$this->getDessenha(),
+			':ID'=>$this->getIdUsuario()
+		));
+	}
+
+	public function __construct($login="", $password="")
+	{
+		$this->setDeslogin($login);
+		$this->setDessenha($password);
+	}
+
+	public function __toString()
+	{
 		return json_encode(array(
 			"idusuario"=>$this->getIdUsuario(),
 			"deslogin"=>$this->getDeslogin(),
@@ -75,6 +144,8 @@ class Usuario{
 		));
 	}
 }
+
+
 
 
 
--- DAO/index.php
+++ DAO/index.php
@@ -5,6 +5,37 @@ require_once("config.php");
 $sql= new Sql();
 $root = new Usuario();
 
-$root->loadById(3);
-echo $root;
+//carrega apenas 1 usuario
+//$root->loadById(3);
+//echo $root;
+
+//carrega uma lista de usuarios
+//$lista =Usuario::getList();
+//echo json_encode($lista);
+
+
+//carregar uma lista d eusuários buscando pelo login
+//$search = Usuario::search("jo");
+
+//echo json_encode($search);
+
+
+//carrega usuario usando login e senha
+//$usuario = new Usuario();
+//$usuario->login("root","123");
+//echo json_encode($usuario);
+
+//Criando um novo usuario
+/*$aluno = new Usuario("aluno", "@alun0");
+
+$aluno->insert();
+
+echo $aluno;*/
+
+$usuario = new Usuario();
+$usuario->loadById(12);
+
+$usuario->update('professor','professor');
+
+echo $usuario;
  ?>
